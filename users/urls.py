f07a5b82fbc7a424008f973a2316a0780565416c
unknown <guna19may2015@gmail.com>
2022-01-08 20:35:52 +0530

    push today's work

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from django.urls.conf import include
 
 from . import views
-
+from .views import UserCreateView
 
 urlpatterns = [
-    path('signup/',views.index,name='signup'),
+    path('signup/',UserCreateView.as_view(),name='signup'),
+    # path('login/',views.index,name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
-def index(self):
-    return render(self,'users/signup.html')
+
+class UserCreateView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/signup.html'
