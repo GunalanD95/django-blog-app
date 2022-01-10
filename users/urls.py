e55750545a7f4ceabd788d93fdb11ea04099984f
unknown <guna19may2015@gmail.com>
2022-01-10 20:33:24 +0530

    added login and signup template

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.urls.conf import include
 
 from . import views
-from .views import UserCreateView
+# from .views import UserCreateView
 
 urlpatterns = [
-    path('signup/',UserCreateView.as_view(),name='signup'),
-    # path('login/',views.lo,name='login'),
+    # path('signup/',UserCreateView.as_view(),name='signup'),
+    path('signup/',views.register,name='signup'),
+    path('login/',views.index,name='logins'),
 ]
--- users/views.py
+++ users/views.py
@@ -8,12 +8,19 @@ from django.views.generic.edit import FormView
 # Create your views here.
 
 
-class UserCreateView(FormView):
-    template_name = 'registration/signup.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+# class UserCreateView(FormView):
+#     template_name = 'registration/sign_up.html'
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
 
-    def form_valid(self, form):
-        form.save()
-        print("form_valid")
-        return super(UserCreateView, self).form_valid(form)
+#     def form_valid(self, form):
+#         form.save()
+#         print("form_valid")
+#         return super(UserCreateView, self).form_valid(form)
+
+def register(request):
+    return render(request, 'registration/sign_up.html')
+
+
+def index(request):
+    return render(request, 'registration/index.html')
