1c6da3fbc41f758373fe1ec43fc43200ca381cb0
unknown <guna19may2015@gmail.com>
2022-01-10 21:25:04 +0530

    pushing recent changes

--- users/models.py
+++ users/models.py
@@ -3,7 +3,4 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
 
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
-
+from .forms import CreateUserForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -19,7 +20,19 @@ from django.views.generic.edit import FormView
 #         return super(UserCreateView, self).form_valid(form)
 
 def register(request):
-    return render(request, 'registration/sign_up.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        print("request.POST:", request.POST)
+        if form.is_valid():
+            print("form_valid gunalan")
+            form.save()
+            user = form.save(commit=False)
+            user.is_active = True    # if you want to skip email validation
+            user.email = User.objects.normalize_email(user.email)
+            user.save()
+    context = {'form': form}
+    return render(request, 'registration/sign_up.html',context)
 
 
 def index(request):
