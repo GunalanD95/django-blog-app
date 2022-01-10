28a870a6dd67797900c4366f002c05b960a3b421
unknown <guna19may2015@gmail.com>
2022-01-10 14:55:28 +0530

    login views

--- users/views.py
+++ users/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 
 
 # Create your views here.
 
 
-class UserCreateView(generic.CreateView):
+class UserCreateView(FormView):
+    template_name = 'registration/signup.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'users/signup.html'
+
+    def form_valid(self, form):
+        form.save()
+        print("form_valid")
+        return super(UserCreateView, self).form_valid(form)
